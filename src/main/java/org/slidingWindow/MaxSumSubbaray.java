24e5e3c3afb0b4e756709d305836b2d802b2d4f8
sabrina-sahli <sahli.sabrina@gmail.com>
2023-10-13 10:45:24 +0200

    [easy] Max Sum Subarray of size K

--- src/main/java/org/slidingWindow/MaxSumSubbaray.java
+++ src/main/java/org/slidingWindow/MaxSumSubbaray.java
@@ -4,35 +4,32 @@ package org.slidingWindow;
 public class MaxSumSubbaray {
 
 
-  public  static  long maximumSumSubarray(int k, int[] arr , int n){
-        Long ans = 0L;
-        int i=0;
-        int j=0;
-
-        if(n < k){
-            System.out.println(" invalid");
-        }
-        while(i < k){
-         ans += arr[i];
-         i++;
+  public  static  int maximumSumSubarray(int k, int[] arr , int n){
+      int ans = 0;
+      int i=0;
+      if(n < k){
+          System.out.println(" invalid");
       }
-      Long temp = ans.longValue();
+      while(i < k){
+          ans += arr[i];
+          i++;
+      }
+      int temp = ans;
       while(i < n){
-            temp += arr[i];
-            temp -= arr[i-k];
-            ans = Math.max(temp, ans);
-            i++;
-
+          temp += arr[i];
+          temp -= arr[i-k];
+          ans = Math.max(temp, ans);
+          i++;
       }
-        return ans;
+      return ans;
     }
 
 
 
     public static void main(String[] args){
 
-        int[] nums={100, 200, 300, 400};
-        long ans =  maximumSumSubarray(2,nums,4);
-        System.out.println(" ans "+ ans);
+       int[] nums={100, 200, 300, 400};
+       int ans =  maximumSumSubarray(2,nums,4);
+       System.out.println(" ans "+ ans);
     }
 }
