7cc12bcf0b25d1271c3855bfc7770a7773be8879
sabrina-sahli <sahli.sabrina@gmail.com>
2023-11-30 10:31:10 +0100

    [easy][arrays] 26.  remove duplicates revision

--- src/main/java/org/arrays/Problem_26_Remove_Duplicates.java
+++ src/main/java/org/arrays/Problem_26_Remove_Duplicates.java
@@ -13,6 +13,18 @@ public class Problem_26_Remove_Duplicates {
         return index;
     }
 
+    public static int removeDuplicates2(int[] nums) {
+
+        int index =0;
+        for(int i=1;i< nums.length;i++){
+            if (nums[i] != nums[index]) {
+                index++;
+                nums[index]= nums[i];
+            }
+        }
+        return index+1;
+    }
+
     public static void main(String[]args){
 
         int[] nums = {1,1,2};
